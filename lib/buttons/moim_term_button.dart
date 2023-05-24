bf33a9c871cbb3eece13637d2bd931178f62021d
minhyeok <sun98034@gmail.com>
2023-05-24 16:30:33 +0900

    textformfield need update

--- lib/buttons/moim_term_button.dart
+++ lib/buttons/moim_term_button.dart
@@ -21,7 +21,7 @@ class _MoimTermButtonState extends State<MoimTermButton> {
         children: [
           Padding(
             padding: const EdgeInsets.only(left: 16.0),
-            child: Text('모임 주기를 선택해주세요',style: TextStyle(fontWeight: FontWeight.w500,fontSize: 16,fontFamily: 'SUIT', color: MIXIN_BLACK_4),),
+            child: Text('모집 기간을 선택해주세요',style: TextStyle(fontWeight: FontWeight.w500,fontSize: 16,fontFamily: 'SUIT', color: MIXIN_BLACK_4),),
           ),
           Padding(
             padding: const EdgeInsets.only(right: 4.0),
